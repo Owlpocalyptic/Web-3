f753fb250e88ce734c4387cf4faa533ef1d34294
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-03-08 11:05:09 +1300

    Programmatized the navbar, whooo

--- Visualizations/app.py
+++ Visualizations/app.py
@@ -5,29 +5,44 @@ from flask import render_template
 
 app = Flask(__name__)
 
+pages = [['Home', 'Home'], ['Inspirations', 'Inspirations'], ['Waluigi', 'Testing Grounds'], ['Void', 'The Other Void']]
+
 @app.route('/')
 @app.route('/home')
 @app.route('/Home')
 @app.route('/Index')
 @app.route('/index')
-def index():
+def Home():
     title = 'Home'
-    return render_template('index.html', title=title)
+    header = 'Project Hootlife'
+    linkName = 'Home'
+    return render_template('index.html', title=title, header=header, linkName=linkName, pages=pages)
 
 @app.route('/Inspirations')
 @app.route('/insp')
 @app.route('/Insp')
 @app.route('/inspirations')
-def inspirations():
+def Inspirations():
     title = 'Inspirations'
-    return render_template('inspirations.html', title=title)
+    header = 'Inspirations'
+    linkName = 'Inspirations'
+    return render_template('inspirations.html', title=title, header=header, linkName=linkName, pages=pages)
 
 @app.route('/Waluigi')
 @app.route('/waluigi')
 @app.route('/ourlordandsaviour')
-def waluigiPage():
+def Waluigi():
     title = 'WAAAAAAAAA'
-    return render_template('jquery.html', title=title)
-    
+    header = 'Z O I N K S'
+    linkName = 'Waluigi'
+    return render_template('jquery.html', title=title, header=header, linkName=linkName, pages=pages)
+
+@app.route('/Void')
+def Void():
+    title = 'THE VOID ETERNAL'
+    header = ''
+    linkName = 'Void'
+    return render_template('jquery.html', title=title, header=header, linkName=linkName, pages=pages)
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
